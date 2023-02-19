343e2f4c24979bcae4401a4a60f4a66a46f6653b
jtambe <tambe_jayesh@hotmail.com>
2023-02-19 00:28:24 -0800

    Added templates for members app

--- DjangoWebsite/DjangoWebsite/settings.py
+++ DjangoWebsite/DjangoWebsite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fkgme@a2eppe_ws(7j%4cohi02hb0_w-)ln)d*v417&9pgkml)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['yourdomain.com']
 
 
 # Application definition
