6e0dfa32c7e673b6ab5579393d19c612c6019db7
jtambe <tambe_jayesh@hotmail.com>
2023-02-18 20:51:13 -0800

    models created and table created

--- DjangoWebsite/members/models.py
+++ DjangoWebsite/members/models.py
@@ -1,3 +1,7 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+
