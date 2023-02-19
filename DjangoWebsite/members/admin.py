9a6de5925c21c0623baaeda097542c3c6b99937d
jtambe <tambe_jayesh@hotmail.com>
2023-02-19 00:54:26 -0800

    Django Admin section completed

--- DjangoWebsite/members/admin.py
+++ DjangoWebsite/members/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+
+from django.contrib import admin
+from .models import Member
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+
+admin.site.register(Member, MemberAdmin)
